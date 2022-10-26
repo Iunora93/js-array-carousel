6aae23947aaee951d37a535d35da5592250de225
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-26 16:50:06 +0200

    QueryselectorAll

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 `use strict`
+const images = ["./img/01.jpg", "./img/01.jpg", "./img/01.jpg", "./img/01.jpg", "./img/01.jpg"];
 
+for (let i = 0; i < images.length; i++) {
+    `<div class="item"><img src="${images[i]}"></div>`
+    document.querySelectorAll(".item").innerHTML = ${images[i]}; 
+}
