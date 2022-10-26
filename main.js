7fac3f26485b30756b3a588ad9041d2bbb7dd955
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-26 17:11:02 +0200

    Cambio classe con id items

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 const images = ["./img/01.jpg", "./img/01.jpg", "./img/01.jpg", "./img/01.jpg", "./img/01.jpg"];
 
 for (let i = 0; i < images.length; i++) {
-    `<div class="item"><img src="${images[i]}"></div>`
+    `<div class="item"><img src="${images[i]}"></div>`;
     document.querySelectorAll(".item").innerHTML = ${images[i]}; 
 }
