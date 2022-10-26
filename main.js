7a0288954e862434bd538a7066b64b23dfd3763a
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-26 17:52:47 +0200

    Let active

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 `use strict`
-const images = ["./img/01.jpg", "./img/01.jpg", "./img/01.jpg", "./img/01.jpg", "./img/01.jpg"];
+const images = ["./img/01.jpg", "./img/02.jpg", "./img/03.jpg", "./img/04.jpg", "./img/05.jpg"];
 
+let containerItems = document.getElementById("items");
 for (let i = 0; i < images.length; i++) {
-    `<div class="item"><img src="${images[i]}"></div>`;
-    document.querySelectorAll(".item").innerHTML = ${images[i]}; 
+    const img = `<div class="item"><img src="${images[i]}"></div>`;
+    containerItems.innerHTML += img; 
 }
+
+let active = 0;
+active = active + 1;
+
